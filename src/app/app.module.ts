fa7caf46120743f657f5a6ce9e508143fa95b1bc
Roman Malilnichenko <malinichenkor@mydigicode.com>
2019-04-09 17:24:17 +0300

    init app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { environment } from '../environments/environment';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+      RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
